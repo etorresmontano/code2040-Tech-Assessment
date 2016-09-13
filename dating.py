feb389ef92e0c7053c5394581ffe675f1814d187
etorresmontano <etorresmontano@gmail.com>
2016-09-13 03:15:26 -0700

    Update dating.py

--- dating.py
+++ dating.py
@@ -18,23 +18,23 @@ github_url = 'https://github.com/etorresmontano/code2040-Tech-Assessment'
 payload = {'token':tokenID}
 
 r = requests.post(host_url, data=payload)
-print r
 dictionary = r.json()
-print dictionary
 interval_seconds = dictionary['interval']
+
+#split up date and time for easier formating
 date = dictionary['datestamp'].split('T')[0].split('-')
 time = dictionary['datestamp'].split('T')[1].split(':')
-print date
-print time
 
+#create a datetime object, minus time zone information. split the second portion to get rid of 'Z'
 stamp = datetime.datetime(int(date[0]),int(date[1]),int(date[2]),int(time[0]),int(time[1]), int(time[2][0:2]))
-print stamp
-stampy = stamp + timedelta(seconds = int(interval_seconds))
-dating = str(stampy.isoformat()) + "Z"
 
-print type(dating)
+#add time to date
+stampy = stamp + timedelta(seconds = int(interval_seconds))
+
+#python does not include tz info in the format needed
+dating = str(stampy.isoformat()) + time[2][2]
+
 payload = {'token':tokenID, 'datestamp':dating}
-print payload
 
 r = requests.post(return_url, data=payload)
 print r.text
